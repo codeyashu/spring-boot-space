bc74bf587a078ba2cf8f440093d2fc6a97bf8837
Rahul Singh <learn.rahulsingh@gmail.com>
2020-11-04 23:26:41 +0530

    spring gemfire cache integration

---
+++ spring-gemfire-cache-demo/src/main/java/io/rsi/codespace/springgemfirecachedemo/SpringGemfireCacheDemoApplication.java
@@ -0,0 +1,14 @@
+package io.rsi.codespace.springgemfirecachedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGemfireCacheDemoApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGemfireCacheDemoApplication.class, args);
+    }
+
+}
---
+++ spring-gemfire-cache-demo/src/test/java/io/rsi/codespace/springgemfirecachedemo/SpringGemfireCacheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rsi.codespace.springgemfirecachedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGemfireCacheDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
