beaa2795e9a552d9db80178352bbdd7caa0a9e6b
Rahul Singh <learn.rahulsingh@gmail.com>
2020-11-05 00:17:04 +0530

    spring rest practice skeleton

---
+++ spring-rest-practice/src/main/java/io/rsi/codespace/springrestpractice/resource/HelloWorldController.java
@@ -0,0 +1,19 @@
+package io.rsi.codespace.springrestpractice.resource;
+
+import io.rsi.codespace.springrestpractice.model.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+}
---
+++ spring-rest-practice/src/test/java/io/rsi/codespace/springrestpractice/SpringRestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rsi.codespace.springrestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
